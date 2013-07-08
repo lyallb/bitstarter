df5f696e08966bb6c6b76a51e4275e6ae8424972
Ubuntu <ubuntu@ip-172-31-41-179.us-west-2.compute.internal>
2013-07-08 08:23:59 +0000

    Added readFileSync

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
+// web.js
+// 20130708
+
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var b=fs.readFileSync('./index.html');
+  //response.send('Hello World2!');
+  response.send(b.toString());
 });
 
 var port = process.env.PORT || 5000;
